b9a6ee1a8fa5b2e0bdd3b1655c29cf6b5cc0606a
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2019-10-26 20:09:35 -0400

    reworked everything and started over, prints header and date from moment, html slightly restructered

--- script.js
+++ script.js
@@ -1,37 +1,9 @@
-$(document).ready(function() {
+var firstHour = 9;
+var lastHour = 18;
 
+function todaysDate() {
+  var todaysDate = moment().format('MMMM Do YYYY');
+  $("#currentDay").append(`<div>${todaysDate}</div>`);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+todaysDate();
